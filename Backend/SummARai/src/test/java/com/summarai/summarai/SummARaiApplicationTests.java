9f35b204074249497b1d2a311ca825de1a792c7c
momen <momenmostafa585@gmail.com>
2025-03-19 14:35:08 +0200

    Creating project, initialize packages, and handling dependencies.

---
+++ Backend/SummARai/src/test/java/com/summarai/summarai/SummARaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.summarai.summarai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SummARaiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
