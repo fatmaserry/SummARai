68d0729bd6ccf3c0c480d0797684c625ebef8ed5
abdelrhmannezzat <abdelrhmanakeel@gmail.com>
2025-05-01 13:13:28 +0300

    Creating files and Adding fulltext and getChunks endpoints (Still in progress)

---
+++ Model/ModelPreprocessing/main.py
@@ -0,0 +1,128 @@
+from fastapi import FastAPI, UploadFile
+import os
+from pathlib import Path
+from mistralai import Mistral
+import re
+from pylatexenc.latex2text import LatexNodes2Text
+from transformers import AutoTokenizer
+#from camel_tools.tokenizers.sentences import SentenceTokenizer
+import spacy
+
+tokenizer = AutoTokenizer.from_pretrained("aubmindlab/bert-base-arabertv02")
+
+API_KEY = '97jEatGBST7Sjlz1B2peJVvjstAvrlCW'
+nlp = spacy.blank('ar')
+nlp.add_pipe('sentencizer')
+
+app = FastAPI()
+
+
+class OCRService:
+    def __init__(self, api_key):
+        self.client = Mistral(api_key=api_key)
+
+    async def process_pdf(self, file: UploadFile) -> str:
+        uploaded_pdf = self.client.files.upload(
+            file={
+                "file_name": file.filename,
+                "content": await file.read()
+            },
+            purpose="ocr"
+        )
+        self.client.files.retrieve(file_id=uploaded_pdf.id)
+        signed_url = self.client.files.get_signed_url(file_id=uploaded_pdf.id)
+        ocr_response = self.client.ocr.process(
+            model="mistral-ocr-latest",
+            document={
+                "type": "document_url",
+                "document_url": signed_url.url
+            },
+            include_image_base64=True
+        )
+        ret = ""
+        for page in ocr_response.pages:
+            temp = LatexNodes2Text().latex_to_text(page.markdown)
+            temp = re.sub(r'!\[.*?\]\(.*?\)', '', temp)
+            # temp = temp.replace("\n", " ")
+            ret += temp
+
+        return ret
+
+    # Not efficient, chunks are split according to number of tokens without respect to ending of a sentence
+    async def chunk_by_max_tokens(self, text: str, max_tokens=1024):
+        words = text.split()
+        chunks = []
+        chunk = []
+        token_count = 0
+        for word in words:
+            tokens = tokenizer.tokenize(word)
+            if token_count + len(tokens) > max_tokens:
+                chunks.append(' '.join(chunk))
+                chunk = []
+                token_count = 0
+            chunk.append(word)
+            token_count += len(tokens)
+        if chunk:
+            chunks.append(' '.join(chunk))
+        return chunks
+
+    # Very good, but not always efficient especially with dialogues
+    async def chunk_by_sentences(self, text, max_sentences=5):
+        sentences = re.split(r'(?<=[.؟!])\s+', text)
+        return [' '.join(sentences[i:i + max_sentences]) for i in range(0, len(sentences), max_sentences)]
+
+
+    # using spacy library
+    async def chunk_using_spacy(self,text, max_sentences=5):
+        doc = nlp(text)
+        sentences = [sent.text for sent in doc.sents]
+        return [' '.join(sentences[i:i + max_sentences]) for i in range(0, len(sentences), max_sentences)]
+
+
+ocr_service = OCRService(api_key=API_KEY)
+
+
+@app.post("/full-text")
+async def get_full_text(file: UploadFile):
+    text = await ocr_service.process_pdf(file)
+    return {"text": text}
+
+
+@app.post("/chunk-tokens")
+async def get_chunk_tokens(file: UploadFile):
+    text = await ocr_service.process_pdf(file)
+    temp = await ocr_service.chunk_by_max_tokens(text)
+    return {
+        "chunks": temp[0],
+        "size": len(temp)
+    }
+
+
+@app.post("/chunk-sentences")
+async def get_chunk_sentences(file: UploadFile):
+    text = await ocr_service.process_pdf(file)
+    temp = await ocr_service.chunk_by_sentences(text)
+    return {
+        "chunks": temp[3],
+        "ch": temp[4],
+        "c": temp[5],
+        "size": len(temp)
+    }
+
+
+
+@app.post("/chunk-spacy")
+async def get_chunk_sentences_spacy(file: UploadFile):
+    text = await ocr_service.process_pdf(file)
+    temp = await ocr_service.chunk_using_spacy(text)
+    return {
+        "first": temp[0],
+        "second": temp[1],
+        "third": temp[2],
+        "fourth": temp[3],
+        "fifth": temp[4],
+        "sixth": temp[5],
+        "size": len(temp)
+    }
+
+
